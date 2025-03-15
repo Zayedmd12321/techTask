6795cd4c130ca26ea9dd9d4cd94a214011c60bb5
Md Zayed Ghanchi <eagle.zayed@gmail.com>
2025-03-15 22:14:12 +0530

    Fix issues

--- script.js
+++ script.js
@@ -6,10 +6,16 @@ const gameArea = screenHeight-baseHeight;
 
 let lowerPipe = document.querySelector('.lower-pipe');
 let upperPipe = document.querySelector('.upper-pipe');
+let score = 0;
 
 lowerPipe.addEventListener('animationiteration',() => {
     const offset = parseInt(gameArea*0.3);
-    let lowerPipe_height = parseInt(Math.random()*(gameArea-1.2*offset)+30)
-    lowerPipe.style.height = `${lowerPipe_height}px`
-    upperPipe.style.height = `${gameArea-lowerPipe_height-offset+10}px`
+    if (score>0){
+        lowerPipe.style.right = '-52px'
+        upperPipe.style.right = '-52px'
+    }
+    let lowerPipe_height = parseInt(Math.random()*(gameArea-1.2*offset)+30);
+    lowerPipe.style.height = `${lowerPipe_height}px`;
+    upperPipe.style.height = `${gameArea-lowerPipe_height-offset+10}px`;
+    score+=1;
 });
