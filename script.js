9bfd86b836baff37b532de8a03fe090c9783419d
Md Zayed Ghanchi <eagle.zayed@gmail.com>
2025-03-15 23:22:10 +0530

    WelcomeScreen Added

--- script.js
+++ script.js
@@ -4,18 +4,47 @@ const baseHeight = 110;
 
 const gameArea = screenHeight-baseHeight;
 
-let lowerPipe = document.querySelector('.lower-pipe');
-let upperPipe = document.querySelector('.upper-pipe');
+const gameContainer = document.querySelector('.game-container')
+
 let score = 0;
 
-lowerPipe.addEventListener('animationiteration',() => {
-    const offset = parseInt(gameArea*0.3);
-    if (score>0){
-        lowerPipe.style.right = '-52px'
-        upperPipe.style.right = '-52px'
+window.onkeydown = (e) => {
+    if (e.keyCode == 32) {
+        jump();
     }
-    let lowerPipe_height = parseInt(Math.random()*(gameArea-1.2*offset)+30);
+};
+
+function jump() {
+    initializePipes();
+}
+
+function initializePipes() {
+    if (!document.querySelector('.lower-pipe')) {
+        gameContainer.innerHTML = `
+            <div id="player"></div>
+            <div class="lower-pipe"></div>
+            <div class="upper-pipe"></div>
+            <div id="base"></div>
+        `;
+
+        setupPipeAnimation();
+    }
+}
+
+function setupPipeAnimation() {
+    const lowerPipe = document.querySelector('.lower-pipe');
+    const upperPipe = document.querySelector('.upper-pipe');
+
+    lowerPipe.addEventListener('animationiteration', () => {
+        updatePipePositions(lowerPipe, upperPipe);
+    });
+}
+
+function updatePipePositions(lowerPipe, upperPipe) {
+    const offset = parseInt(gameArea * 0.3);
+    let lowerPipe_height = parseInt(Math.random() * (gameArea - 1.2 * offset) + 30);
     lowerPipe.style.height = `${lowerPipe_height}px`;
-    upperPipe.style.height = `${gameArea-lowerPipe_height-offset+10}px`;
-    score+=1;
-});
+    upperPipe.style.height = `${gameArea - lowerPipe_height - offset + 10}px`;
+
+    score += 1;
+}
